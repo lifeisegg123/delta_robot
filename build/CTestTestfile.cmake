15593158d765fb0c97e454a1f53afd3ec19f3467
LJJ <leejj2002@naver.com>
2020-06-28 23:47:24 +0900

    remove

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lee/Desktop/workspace/delta_robot
-# Build directory: /home/lee/Desktop/workspace/delta_robot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
